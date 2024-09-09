f88a9192a102a75bac6e7d0540b5357ad4ef61ee
bilal <m-bilaljaved612@gmail.com>
2024-09-09 18:43:28 +0500

    scrol function

--- js/main.js
+++ js/main.js
@@ -16,6 +16,29 @@ window.onload = function () {
         document.querySelector('.pre-loader').remove();
     }, 1200);
 };
+// scroll 
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 // Counter Element
 function animateCounter(element, start, end, duration) {
     let startTime = null;
